0ea7b9643491c9e363bfd80e3956a4b8fdd434b0
igorbzi <igorbazei.ilb@gmail.com>
2024-03-20 00:28:11 -0300

    Ajustes finais

--- read_file.c
+++ read_file.c
@@ -172,13 +172,3 @@ int read_file(char consulta[20]){
     
     return 0;
 }
-
-int main(int argc, char *argv[]){
-
-    if(argc>1){
-        read_file(argv[1]);
-        return 0;
-    }
-    printf("LOGICAL TABLE NAME MISSING\n");
-    return 0;
-}
