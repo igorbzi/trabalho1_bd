63516e22836395946dff726f58595bf4baa58952
igorbzi <igorbazei.ilb@gmail.com>
2024-03-19 23:29:41 -0300

    Adição do main e da entrada pelo argce argv

--- read_file.c
+++ read_file.c
@@ -17,7 +17,9 @@ typedef struct Atributo{
 } Atributo;
 
 int encontra_tabela(Tabela *tabela, char *consulta){
+
     FILE *ft = fopen("table.dic", "rb");
+
     while(getc(ft) != EOF){
         fseek(ft, -1, SEEK_CUR);
         fread(tabela, sizeof(Tabela), 1, ft);
@@ -26,13 +28,16 @@ int encontra_tabela(Tabela *tabela, char *consulta){
             return 0;
         }
     }
+
     return 1;
-};
+}
 
 int encontra_atributos(Tabela tabela, Atributo *atributos){
+
     FILE *fe = fopen("att.dic", "rb");
     Atributo aux;
     int i = 0;
+
     while(getc(fe) != EOF){
         fseek(fe, -1, SEEK_CUR);
         fread(&aux, sizeof(Atributo), 1, fe);
@@ -41,10 +46,11 @@ int encontra_atributos(Tabela tabela, Atributo *atributos){
             i++;
         }
     }
+
     return i;
 }
 
-int imprime_dados(char *arquivo, Atributo *lista_att, int qtd_att){
+void imprime_dados(char *arquivo, Atributo *lista_att, int qtd_att){
     
     char letra;
     char *string;
@@ -55,14 +61,18 @@ int imprime_dados(char *arquivo, Atributo *lista_att, int qtd_att){
 
     while(getc(fd) != EOF){
         fseek(fd, -1, SEEK_CUR);
+
         printf("|");
-        for(int i=0;i<qtd_att; i++){
+
+        for(int i = 0; i < qtd_att; i++){
             switch(lista_att[i].tipo){
+
                 case 'C':
                     fread(&letra, sizeof(char), 1, fd);
                     printf(" %*c ", (int)strlen(lista_att[i].nome), letra);
                     printf("|");
                     break;
+
                 case 'S':
                     string = malloc(lista_att[i].tam);
                     fread(string, lista_att[i].tam, 1, fd);
@@ -74,6 +84,7 @@ int imprime_dados(char *arquivo, Atributo *lista_att, int qtd_att){
                     printf("|");
                     free(string); 
                     break;
+
                 case 'I':
                     fread(&num_int, sizeof(int), 1, fd);
                     if((int)strlen(lista_att[i].nome)>lista_att[i].tam){
@@ -83,28 +94,32 @@ int imprime_dados(char *arquivo, Atributo *lista_att, int qtd_att){
                     }
                     printf("|");
                     break;
+
                 case 'D':
                     fread(&num_double, sizeof(double), 1, fd);
                     printf(" %-*.4lf ", 8, num_double);
                     printf("|");
                     break;
+
                 case 'F':
                     fread(&num_float, sizeof(float), 1, fd);
                     printf(" %-*.4f ", 8, num_float);
                     printf("|");
                     break;
+
             }
         }
         printf("\n");
     }
     
     fclose(fd);
-    return 0;
+    return;
 }
 
-int imprime_cabecalho(Atributo *atributos, int qtd_att){
+void imprime_cabecalho(Atributo *atributos, int qtd_att){
     printf("|");
-    for(int i=0;i<qtd_att;i++){
+
+    for(int i = 0; i < qtd_att; i++){
         if((int)strlen(atributos[i].nome)>atributos[i].tam){
             printf(" %-*s |", (int)strlen(atributos[i].nome), atributos[i].nome);
         } else {
@@ -112,38 +127,42 @@ int imprime_cabecalho(Atributo *atributos, int qtd_att){
         }
     }
     printf("\n");
-    return 0;
+    return;
 }
 
-int main(){
+int read_file(char consulta[20]){
     int retorno_tab, retorno_att;
-    char consulta[20];
     Tabela tabela;
     Atributo atributos[25];
-
-    scanf("%s", consulta);
 
     retorno_tab = encontra_tabela(&tabela, consulta);
 
     if(retorno_tab){
-        printf("tabela nao encontrada!\n");
-        
-    } else {
-        //printf("\n****TABELA****\n\n");
-        //printf("id: %d\ntabela: %s\narquivo: %s\n\n\n", tabela.cod, tabela.logico, tabela.fisico);
-        retorno_att = encontra_atributos(tabela, atributos);
-        if(!retorno_att){
-            printf("esquema nao encontrado!\n");
-            return 0;
-        }
-        
-        // for(int i=0;i<retorno_att;i++){
-        //     printf("id: %d\nnome: %s\ntipo: %c\nop: %d\ntamanho: %d\n\n", atributos[i].id, atributos[i].nome, atributos[i].tipo, atributos[i].op, atributos[i].tam);
-        // }
 
-        imprime_cabecalho(atributos, retorno_att);
-        imprime_dados(tabela.fisico, atributos, retorno_att);
-
+        printf("TABLE NOT FOUND\n");
+        return 0;
     }
+
+    retorno_att = encontra_atributos(tabela, atributos);
+    
+    if(!retorno_att){
+        printf("SCHEMA NOT FOUND\n");
+        return 0;
+    }
+
+    imprime_cabecalho(atributos, retorno_att);
+    imprime_dados(tabela.fisico, atributos, retorno_att);
+
+    
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+
+    if(argc>1){
+        read_file(argv[1]);
+        return 0;
+    }
+    printf("LOGICAL TABLE NAME MISSING\n");
     return 0;
 }
